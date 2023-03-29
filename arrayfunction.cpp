9fa7f36f3311594d5134993540362e6d3c6e93d9
Kuldeep8410 <118438689+Kuldeep8410@users.noreply.github.com>
2023-03-29 21:15:37 +0530

    Add files via upload

---
+++ arrayfunction.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{ int avg, sum = 0 ;
+int i ;
+int marks[30] ; /* array declaration */
+for ( i = 0 ; i <= 29 ; i++ )
+{ printf ( "\nEnter marks " ) ;
+scanf ( "%d", &marks[i] ) ;
+}
+    
+    
+}
